ba17408f5595928caddbb7d1a6d0ca980b2ad144
AK4598 <56329376+hkbcps040029@users.noreply.github.com>
2021-10-21 22:00:55 +0800

    1021_change

--- faces_gui_test.py
+++ faces_gui_test.py
@@ -7,7 +7,7 @@ import pickle
 from datetime import datetime
 import sys
 import PySimpleGUI as sg
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
 # remember to "pip install PTable"
 
 globalCurrentName = 'Jack'
@@ -15,7 +15,6 @@ globalCurrentID = 1003
 
 # 1 Create database connection
 myconn = mysql.connector.connect(host="localhost", user="root", passwd="123456", database="face3278")
-
 
 date = datetime.utcnow()
 date = date.strftime('%Y-%m-%d %H:%M:%S')
@@ -49,28 +48,27 @@ cap = cv2.VideoCapture(0)
 # 3 Define pysimplegui setting
 # Small gui window
 layout =  [
-    [sg.Text('Setting', size=(18,1), font=('Any',18),text_color='#1c86ee' ,justification='left')],
-    [sg.Text('Confidence', key='cfd'), sg.Slider(range=(0,100),orientation='h', resolution=1, default_value=60, size=(15,15), key='confidence')],
-    [sg.OK(), sg.Cancel(), sg.Button('Test')]
+    [sg.Text('Welcome to ABC Bank! Do you want to start recognition?', size=(50,1), font=('Any',18), text_color='#ffffff', justification='center')],
+    [sg.Text('Confidence', key='cfd', visible=False), sg.Slider(range=(0,100),orientation='h', resolution=1, default_value=60, size=(15,15), key='confidence', visible=False)],
+    [sg.Button('Yes'), sg.Button('No')]
       ]
-win = sg.Window('iKYC System',
-        default_element_size=(21,1),
-        text_justification='right',
-        auto_size_text=False).Layout(layout)
-print("I'm here1")
-event, values = win.Read()
-if event is None or event =='Cancel':
-    print("I'm here")
-    exit()
-if event == 'Test':
-    win.Element('cfd').update('Deviance')
-    win.Element('confidence').update(visible=False)
-    print("Reached")
-
+win = sg.Window('ABC Bank Login',
+        element_justification='c',
+        default_element_size=(200,100),
+        text_justification='center',
+        auto_size_text=True).Layout(layout)
 
 event, values = win.Read()
-if event is None or event =='Cancel':
+
+if event == 'Yes':
+    win.Close()
+
+elif event == 'No':
     exit()
+
+# if event is None or event =='Cancel':
+#     exit()
+
 
 args = values
 gui_confidence = args["confidence"]
@@ -82,8 +80,11 @@ print("I'm there")
 
 breakCount = 0
 
+recognize_face = True
+
 # 4 Open the camera and start face recognition
-while True:
+# while True:
+while recognize_face:
     # breakCount = -1
     # break # self added
     ret, frame = cap.read()
@@ -139,33 +140,22 @@ while True:
 
 
                 """
-                #cap.release()
-                #cv2.destroyAllWindows()
-                # breakCount = -1
-                # break
 
-                update =  "UPDATE Login_History SET date_time='%s' WHERE customer_id=%i" % (date, data[0])
-                # val = (date, data[0])
+
+                update =  "INSERT INTO Login_History VALUES ('%s', %i)" % (date, data[0])
                 cursor.execute(update)
-                # update = "UPDATE Customer SET login_time=%s WHERE name=%s"
-                # val = (current_time, current_name)
-                # cursor.execute(update, val)
-                # myconn.commit()
-
-                # hello = ("Hello ", current_name, "Welcom to the iKYC System")
+                myconn.commit()
                 hello = ("Hello ", globalCurrentName, "Welcom to the Bank ABC", "\nYour current login time is: %s" % (date))
-                # hello = ("Hello ", globalCurrentName, "Welcom to the Bank ABC", "\nYour current login time is: %s" %(date))
-                # print(hello)
                 engine.say(hello)
 
-                select_history = "SELECT * FROM Login_History WHERE customer_id=%i ORDER BY date_time ASC" % (data[0])
-                # val = (data[0])
-                # cursor.execute(select_history, val)
-                # print(select_history)
+
+
+                select_history = "SELECT date_time FROM Login_History WHERE customer_id=%i ORDER BY date_time DESC" % (data[0])
                 cursor.execute(select_history)
-                # myconn.commit()
                 history = cursor.fetchall()
-                break
+
+            
+                recognize_face = False
 
                 # table way
                 # history_table = PrettyTable()
@@ -194,9 +184,6 @@ while True:
             cv2.putText(frame, "UNKNOWN", (x, y), font, 1, color, stroke, cv2.LINE_AA)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), (2))
             hello = ("Your face is not recognized")
-            # cap.release()
-            # cv2.destroyAllWindows()
-            print(hello)
             engine.say(hello)
             # engine.runAndWait()
 
@@ -220,37 +207,48 @@ while True:
                 text_justification='right',
                 auto_size_text=False).Layout(layout).Finalize()
         image_elem = win.FindElement('_IMAGE_')
+
     else:
         image_elem.Update(data=imgbytes)
 
+
     event, values = win.Read(timeout=20)
+
+
     if event is None or event == 'Exit':
         break
     gui_confidence = values['confidence']
 
+
+
 win.Close()
 cap.release()
 
-if breakCount != -1:
-    print("Premature closing")
-    exit(0)
+# if breakCount != -1:
+#     print("Premature closing")
+#     exit(0)
 
 # Start Experimenting
-print("Yay I broke free")
 
+hello_msg_1 = 'Hello %s! Welcome to Bank ABC!' % (globalCurrentName)
+hello_msg_2 = 'Your current login time is: %s' % (date)
+
+all_history = [i[0] for i in history]
 
 layout = [
-    [sg.Text('iKYC System Interface', size=(90, 30), key='Title', justification='center')],
-    [sg.Button('Account View'), sg.Button('Transaction List'), sg.Button('Back'), sg.Exit()],
-    [sg.Text('Confidence', key='conTitle'),
-        sg.Slider(range=(0, 100), orientation='h', resolution=1, default_value=60, size=(15, 15), key='confidence')],
-    [sg.Button('Show Login History', key='__HISTORY__')]
+    [sg.Text(hello_msg_1, size=(50, 1), font=('Any',24), key='msg1', justification='center')],
+    [sg.Text(hello_msg_2, size=(50, 1), font=('Any',24), key='msg2', justification='center')],
+    [sg.Listbox(values=all_history, key='_HISTORY_' , size=(18,10), font=('Any',24), select_mode='extended', visible=False)],
+    [sg.Text('', size=(50, 1), font=('Any',24), key='buffer', justification='center')],
+    [sg.Button('Login History'), sg.Button('Account View'), sg.Button('Transaction List'), sg.Exit()]    
 ]
 win = sg.Window('Bank System',
-        default_element_size=(90, 30),
+        element_justification='c',
+        default_element_size=(60, 30),
         text_justification='right',
         auto_size_text=False).Layout(layout)
-print("I'm here2")
+
+
 while True:
     event, values = win.Read()
     if event is None or event == 'Exit':
@@ -258,26 +256,21 @@ while True:
         exit(0)
     if event == 'Account View':
         # Call function here
-        win.Element('Title').Update(visible=False)
-        win.Element('conTitle').Update(visible=False)
-        win.Element('confidence').Update(visible=False)
+        win.Element('msg1').Update(value='%s, this is your account view.' % (globalCurrentName))
+        win.Element('_HISTORY_').Update(visible=False)
 
         cursor.execute("SELECT * FROM Account")
         print(cursor.fetchall())
 
+    if event == 'Login History':
+        win.Element('msg1').Update(value='%s, this is your login history.' % (globalCurrentName))
+        win.Element('msg2').Update(visible=False)
+        win.Element('_HISTORY_').Update(visible=True)
+
 
     if event == 'Transaction List':
-        win.Element('Title').Update(visible=False)
-        win.Element('confidence').Update(visible=False)
-    if event == 'Back':
-        win.Element('Title').Update(visible=True)
-        win.Element('conTitle').Update(visible=True)
-        win.Element('confidence').Update(visible=True)
-
-
-
-
-
+        win.Element('msg1').Update(value='%s, this is your transaction list.' % (globalCurrentName))
+        win.Element('_HISTORY_').Update(visible=False)
 
 
 # ======== Page Navigation Functions =========
